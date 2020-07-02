028f0e4ed36db80dda26c89c48b3d9bfe4101e92
lyhti <tiqmekfn@naver.com>
2020-07-02 10:50:31 +0900

    200702 acorn FinalProject aws/EC2 연동 버전 입니다.

--- README.md
+++
@@ -1,1 +0,0 @@
-# acorn_FinalProject_Spring
---
+++ src/main/java/upload/util/SpringFileWrite.java
@@ -0,0 +1,74 @@
+package upload.util;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
+
+public class SpringFileWrite
+{
+	private FileOutputStream fos;
+
+	public void writeFile(MultipartFile file, String path)
+	{
+
+		String fileName = file.getOriginalFilename();
+
+		try {
+			byte[] fileData = file.getBytes();
+			fos = new FileOutputStream(path+"/"+fileName);
+			fos.write(fileData);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				fos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void writeFile(MultipartFile file, String path, String myid)
+	{
+		String fileName = myid+"_"+file.getOriginalFilename();
+
+		try {
+			byte[] fileData = file.getBytes();
+			fos = new FileOutputStream(path+"/"+fileName);
+			fos.write(fileData);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				fos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void writeFileRename(MultipartFile file, String path, String fileName)
+	{		
+		try {
+			byte[] fileData = file.getBytes();
+			fos = new FileOutputStream(path+"/"+fileName);
+			fos.write(fileData);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				fos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
